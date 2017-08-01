16a71492b10cef41927e2dedf32c24760b548b02
jomonkjoy <jomonkurianjoy@gmail.com>
2017-08-01 22:00:55 +0530

    Update pcie_packages.sv

--- pcie_packages.sv
+++ pcie_packages.sv
@@ -20,4 +20,15 @@ typedef enum {
   gen3 = 2'b10  // 8Gbps
 } pcie_gen;
 
+parameter COM = K28p5;
+parameter STP = K27p7;
+parameter SDP = K28p2;
+parameter END = K29p7;
+parameter EDB = K30p7;
+parameter PAD = K23p7;
+parameter SKP = K28p0;
+parameter FTS = K28p1;
+parameter IDL = K28p3;
+parameter EIE = K28p7;
+  
 endpackage
