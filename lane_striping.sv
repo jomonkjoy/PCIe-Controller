1094e9bb0ae55fa349d155ff133182be672bed38
jomonkjoy <jomonkurianjoy@gmail.com>
2017-11-23 18:21:11 +0000

    Update lane_striping.sv

--- lane_striping.sv
+++ lane_striping.sv
@@ -1,11 +1,27 @@
 module byte_striping #(
+  parameter integer DATA_LANES = 4
 ) (
+  input  logic [7:0][DATA_LANES-1:0] in,
+  output logic [7:0] out[DATA_LANES]
 );
-
+  
+  genvar i;
+  generate for (i=0; i<DATA_LANES; i++) begin : gen_strip
+    assign out[i] = in[i];
+  end endgenerate
+  
 endmodule
 
-module byte_striping #(
+module byte_destriping #(
+  parameter integer DATA_LANES = 4
 ) (
+  input  logic [7:0] in[DATA_LANES],
+  output logic [7:0][DATA_LANES-1:0] out
 );
-
+  
+  genvar i;
+  generate for (i=0; i<DATA_LANES; i++) begin : gen_destrip
+    assign out[i] = in[i];
+  end endgenerate
+  
 endmodule
