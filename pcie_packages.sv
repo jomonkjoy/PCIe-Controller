94ab2054b53f05ce1a335107231dc1a0ac9aa64b
Jomon K Joy <jomonkurianjoy@gmail.com>
2018-05-23 21:11:11 +0530

    Update pcie_packages.sv

--- pcie_packages.sv
+++ pcie_packages.sv
@@ -20,15 +20,15 @@ typedef enum {
   gen3 = 2'b10  // 8Gbps
 } pcie_gen;
 
-parameter COM = K28p5;
-parameter STP = K27p7;
-parameter SDP = K28p2;
-parameter END = K29p7;
-parameter EDB = K30p7;
-parameter PAD = K23p7;
-parameter SKP = K28p0;
-parameter FTS = K28p1;
-parameter IDL = K28p3;
-parameter EIE = K28p7;
+parameter COM = K28p5; // Comma char ised for lane&link initialization and management
+parameter STP = K27p7; // Marker for start of a Transaction layer packet
+parameter SDP = K28p2; // Marker for start of a Data link layer packet
+parameter END = K29p7; // Marker for end of a DLP/TLP
+parameter EDB = K30p7; // Marker for end of a Bad or nullified TLP
+parameter PAD = K23p7; // pad used in fraiming and link width & lane ordering negotiation
+parameter SKP = K28p0; // skip order set for difference in data-rate btw links
+parameter FTS = K28p1; // fast training sequence used in an ordered set to exit from L0s to L0
+parameter IDL = K28p3; // electrical idle order set
+parameter EIE = K28p7; // electrical idle exit order set
   
 endpackage
